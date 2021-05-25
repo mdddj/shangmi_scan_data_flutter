24b571716680a6e899ed2dbc8a51cadcec44c4da
mddd <413153189@qq.com>
2021-05-25 10:03:50 +0800

    升级到空安全版本

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+# 1.0.0
+* 升级到空安全版本
+
 ## 0.0.1
 
 * TODO: Describe initial release.
--- lib/sm_scan.dart
+++ lib/sm_scan.dart
@@ -6,8 +6,8 @@ class SmScan {
   static const MethodChannel _channel =
       const MethodChannel('sm_scan');
 
-  static Future<String> smScanInit() async {
-    final String code = await _channel.invokeMethod('init');
+  static Future<String?> smScanInit() async {
+    final String? code = await _channel.invokeMethod('init');
     return code;
   }
 }
